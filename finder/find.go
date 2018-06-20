a1608f41444279d4e95a3aea5549e3f0569e156f
Mark van der Velden <mark@dynom.nl>
2018-06-20 14:53:48 +0200

    Testing the tolerance

--- finder/find.go
+++ finder/find.go
@@ -22,6 +22,7 @@ var (
 	ErrNoAlgorithmDefined = errors.New("no algorithm defined")
 )
 
+// WorstScoreValue holds the value of the lowest possible score
 const WorstScoreValue = -1 * math.MaxFloat32
 
 // New creates a new instance of Finder. The order of the list is significant
@@ -47,11 +48,12 @@ func New(list []string, options ...Option) (*Finder, error) {
 }
 
 // Find returns the best alternative a score and if it was an exact match or not.
+// Since algorithms can define their own upper-bound, there is no "best" value.
 func (t Finder) Find(input string) (string, float64, bool) {
 	return t.FindCtx(context.Background(), input)
 }
 
-// FindCtx is the same as Find, with context support
+// FindCtx is the same as Find, with context support.
 func (t Finder) FindCtx(ctx context.Context, input string) (string, float64, bool) {
 	// Initial value, compatible with JSON serialisation. It's not ideal to mix presentation with business logic
 	// but in this instance it was convenient and similarly effective to math.Inf(-1)
@@ -59,18 +61,18 @@ func (t Finder) FindCtx(ctx context.Context, input string) (string, float64, boo
 
 	// Exact matches
 	if _, exists := t.referenceMap[input]; exists {
-		return input, hs, true
+		return input, WorstScoreValue, true
 	}
 
 	var best = input
 	for _, ref := range t.reference {
 		select {
 		case <-ctx.Done():
-			return input, hs, false
+			return input, WorstScoreValue, false
 		default:
 		}
 
-		// Test if the input length is much fewer, making it an unlikely typo.
+		// Test if the input length is much less, making it an unlikely typo.
 		if !meetsLengthTolerance(t.LengthTolerance, input, ref) {
 			continue
 		}
@@ -92,7 +94,8 @@ func meetsLengthTolerance(t float64, input, reference string) bool {
 
 	inputLen := len(input)
 	refLen := len(reference)
+	threshold := int(math.Ceil(float64(inputLen) * t))
 
 	// The result is N% of the length or at least 1 (due to math.Ceil)
-	return inputLen >= refLen-int(math.Ceil(float64(inputLen)*t))
+	return refLen-threshold <= inputLen && inputLen <= refLen+threshold
 }
--- finder/option_test.go
+++ finder/option_test.go
@@ -13,3 +13,56 @@ func TestSetAlgorithm(t *testing.T) {
 		t.Errorf("Expected the algorithm to be set")
 	}
 }
+
+func TestSetTolerance(t *testing.T) {
+	veryPositiveAlg := func(a, b string) float64 {
+		return 1
+	}
+
+	testData := []struct {
+		Input  string
+		Expect string
+		Exact  bool
+	}{
+		// Too long
+		{Input: "coffeeeee", Expect: "coffeeeee", Exact: false},
+
+		// Within the threshold of "2" characters
+		{Input: "coffeeee", Expect: "coffee", Exact: false},
+		{Input: "coffeee", Expect: "coffee", Exact: false},
+		{Input: "coffee", Expect: "coffee", Exact: true},
+		{Input: "coffe", Expect: "coffee", Exact: false},
+
+		// Too short
+		{Input: "coff", Expect: "coff", Exact: false},
+		{Input: "cof", Expect: "cof", Exact: false},
+	}
+
+	for _, td := range testData {
+		sug, _ := New([]string{"coffee"}, WithAlgorithm(veryPositiveAlg), WithLengthTolerance(0.2))
+
+		m, _, e := sug.Find(td.Input)
+		if m != td.Expect {
+			t.Errorf("Expected the best match to equal '%s', instead I got '%s'.", td.Expect, m)
+		}
+
+		if e != td.Exact {
+			t.Errorf("Expected the result to match %t, instead I got %t.", td.Exact, e)
+		}
+	}
+}
+
+func TestSetToleranceDisable(t *testing.T) {
+	veryPositiveAlg := func(a, b string) float64 {
+		return 1
+	}
+
+	expect := "coffee"
+	sug, _ := New([]string{expect}, WithAlgorithm(veryPositiveAlg), WithLengthTolerance(0))
+
+	// The input is a string exceeding the threshold.
+	m, _, _ := sug.Find("coffee" + "eeeeeeeee")
+	if m != expect {
+		t.Errorf("Expected the best match to equal '%s', instead I got '%s'.", expect, m)
+	}
+}
